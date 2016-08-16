9edfd5571f5b412b512fe36b8193f7f6bf18b725
m-schrepel <mschrepel@gmail.com>
2016-08-15 18:59:14 -0700

    commented and ran gofmt

--- echo.go
+++ echo.go
@@ -7,10 +7,13 @@ import (
   "strings"
 )
 
+// No idea yet how to cast a number to a string for concatenation
 func main() {
   fmt.Println(concatenate() / join() * 100, "%")
 }
 
+// had no idea how to know that time.Now was int64 until compilation
+// did not know that Nanoseconds was one of the apis for time until I looked
 func concatenate() (end int64) {
   var s, sep string
   start := time.Now()
@@ -22,6 +25,8 @@ func concatenate() (end int64) {
   return
 }
 
+// did not know that specifying the return (end int64) counted as a declaration
+// and therefor end := time.Since(...) was wrong
 func join()(end int64) {
   start := time.Now()
   strings.Join(os.Args[1:], " ")
