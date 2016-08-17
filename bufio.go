274a96dbf95f361375ba403aa9655f4231b5c30c
m-schrepel <mschrepel@gmail.com>
2016-08-16 20:01:24 -0700

    added comments and formatting via gofmt

--- bufio.go
+++ bufio.go
@@ -1,52 +1,56 @@
 package main
 
 import (
-    "fmt"
-    "io/ioutil"
-    "os"
-    "strings"
-    "sort"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"sort"
+	"strings"
 )
 
 func main() {
-    counts := make(map[string]int)
-    for _, filename := range os.Args[1:] {
-        data, err := ioutil.ReadFile(filename)
-        if err != nil {
-            fmt.Fprintf(os.Stderr, "dup3: %v\n", err)
-            continue
-        }
-        for _, line := range strings.Split(string(data), " ") {
-            counts[line]++
-        }
-    }
-    // Now we attempt a sort. First we must turn values into a slice
-    // since maps are randomized on purpose (can't guarantee order)
-    keys := make([]int, len(counts))
+	// define our initial map where the words are the key and the count is the value
+	counts := make(map[string]int)
+	// for each file input as the args on the command line...
+	for _, filename := range os.Args[1:] {
+		data, err := ioutil.ReadFile(filename)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "dup3: %v\n", err)
+			continue
+		}
+		for _, line := range strings.Split(string(data), " ") {
+			counts[line]++
+		}
+	}
+	// Now we attempt a sort. First we must turn values into a slice
+	// since maps are randomized on purpose (can't guarantee order)
+	keys := make([]int, len(counts))
 
-    // Build out the slice with the values
-    for _, value := range counts {
-      keys = append(keys, value)
-    }
+	// Build out the slice with the values
+	for _, value := range counts {
+		keys = append(keys, value)
+	}
 
-    // sort in descending order
-    sort.Sort(sort.Reverse(sort.IntSlice(keys)))
+	// sort in descending order
+	sort.Sort(sort.Reverse(sort.IntSlice(keys)))
 
-    var reversedCounts = reverse(counts);
+	// get the reversed map
+	var reversedCounts = reverse(counts)
 
-    for _, n := range keys {
-        if n > 1 {
-          fmt.Println("Key:", n, "Value:", reversedCounts[n])
-        }
-    }
+	// for all the keys in the slice / map print out the key and value
+	for _, n := range keys {
+		if n > 1 {
+			fmt.Println("Key:", n, "Value:", reversedCounts[n])
+		}
+	}
 }
 
-// reverse the key/value pairs so we can list the lines by the key of the string 
+// reverse the key/value pairs so we can list the lines by the key of the string
 // which is the number of occurrences
 func reverse(m map[string]int) map[int]string {
-  reversedCounts := make(map[int]string)
-  for k, v := range m {
-    reversedCounts[v] = k
-  }
-  return reversedCounts
+	reversedCounts := make(map[int]string)
+	for k, v := range m {
+		reversedCounts[v] = k
+	}
+	return reversedCounts
 }
