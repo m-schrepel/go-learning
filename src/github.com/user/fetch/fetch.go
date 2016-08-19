b27cf3a6aad06ce2cae4070300b12395b5ea2892
m-schrepel <mschrepel@gmail.com>
2016-08-18 19:39:28 -0700

    Changed to copy instead of giant buffer

--- src/github.com/user/fetch/fetch.go
+++ src/github.com/user/fetch/fetch.go
@@ -2,25 +2,35 @@
 package main
 
 import (
-    "fmt"
-    "io/ioutil"
-    "net/http"
-    "os"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
 )
 
 func main() {
-    for _, url := range os.Args[1:] {
-        resp, err := http.Get(url)
-        if err != nil {
-            fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
-            os.Exit(1)
-        }
-        b, err := ioutil.ReadAll(resp.Body)
-        resp.Body.Close()
-        if err != nil {
-            fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
-            os.Exit(1)
-        }
-        fmt.Printf("%s", b)
-    }
+	for i, url := range os.Args[1:] {
+		// Prepend http to the url if it isn't already there.
+		// No clever way to deal with http vs https at the moment
+		// at least not for this example
+		if strings.HasPrefix(os.Args[i], "http") != true {
+			url = "http://" + url
+		}
+
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+		b, err := io.Copy(os.Stdout, resp.Body)
+		s := resp.StatusCode
+		resp.Body.Close()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+			os.Exit(1)
+		}
+		fmt.Printf("%d", s)
+		fmt.Printf("%v", b)
+	}
 }
